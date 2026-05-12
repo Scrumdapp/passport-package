f5416dc5a0639d6b52fa7e5ec871f2daf7d3fde4
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-05-12 13:25:17 +0200

    Kotlin related fixes

--- src/main/kotlin/com/scrumdapp/passportstarter/PassportAutoConfiguration.kt
+++ src/main/kotlin/com/scrumdapp/passportstarter/PassportAutoConfiguration.kt
@@ -1,24 +1,15 @@
 package com.scrumdapp.passportstarter
 
-import com.scrumdapp.passportstarter.PassportProperties
 import com.scrumdapp.passportstarter.filters.PassportAuthFilter
 import com.scrumdapp.passportstarter.jwk.PassportService
 import com.scrumdapp.passportstarter.jwk.jwtDecoder
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@ConditionalOnClass(SecurityFilterChain::class)
-@ConditionalOnProperty(
-    prefix = "passport",
-    name = ["enabled"],
-    havingValue = "true",
-    matchIfMissing = true
-)
+@EnableConfigurationProperties(PassportProperties::class)
 class PassportAutoConfiguration {
 
     @Bean
--- src/main/kotlin/com/scrumdapp/passportstarter/PassportProperties.kt
+++ src/main/kotlin/com/scrumdapp/passportstarter/PassportProperties.kt
@@ -4,5 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "passport")
 data class PassportProperties(
-    var jwkSetUri: String = ""
+
+    var jwkSetUri: String
 )
